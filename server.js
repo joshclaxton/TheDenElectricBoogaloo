c08bf99afd61cadae7b98bfeb32bdc53c286e053
Joshua Claxton <Joshua.Claxton@strongmind.com>
2019-12-19 17:13:19 -0700

    Player Creation/Movement

--- server.js
+++ server.js
@@ -49,9 +49,7 @@ io.on('connection', function (socket) {
   // when a player moves, update the player data
   socket.on('playerMovement', function (movementData) {
     players[socket.id].x = movementData.x;
-    players[socket.id].y = movementData.y;
-    players[socket.id].rotation = movementData.rotation;
-    // emit a message to all players about the player that moved
+    players[socket.id].y = movementData.y;    // emit a message to all players about the player that moved
     socket.broadcast.emit('playerMoved', players[socket.id]);
   });
 
